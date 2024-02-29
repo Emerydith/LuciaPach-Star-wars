ac436df1127d35dd32003af81a2dd9fab5ce22c0
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-02-29 18:44:56 +0000

    commit

--- src/js/component/planetcard.js
+++ src/js/component/planetcard.js
@@ -1,13 +1,20 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const PlanetCard = ({ planet }) => {
   return (
-    <div className="card">
-      <h2>{planet.name}</h2>
-      <p><strong>Diámetro:</strong> {planet.diameter}</p>
-      <p><strong>Clima:</strong> {planet.climate}</p>
-      {/* Agrega más propiedades del planeta aquí según sea necesario */}
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{planet.name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
